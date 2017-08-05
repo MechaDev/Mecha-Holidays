f7301903322ec543d41c4559bf9737e41764af9f
Travis O'Donnell <MechaDevTravis@gmail.com>
2017-08-04 21:30:49 -0400

    Wrote unit test scripts for all holidays so far.

---
+++ src/com/mechadev/mechaholidays/tests/AbstractTest.java
@@ -0,0 +1,8 @@
+package com.mechadev.mechaholidays.tests;
+
+import java.text.SimpleDateFormat;
+
+public abstract class AbstractTest {
+	protected final int year = 1974;
+	protected SimpleDateFormat simpleDateFormat  = new SimpleDateFormat("MMMM dd, yyyy");
+}
