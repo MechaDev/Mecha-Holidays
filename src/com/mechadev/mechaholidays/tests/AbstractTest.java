677a932b6aeedc47d789aca02dbc1a78a6f07ba6
Travis O'Donnell <MechaDevTravis@gmail.com>
2017-08-14 21:31:45 -0400

    Removed JUnit scripts from repository

--- src/com/mechadev/mechaholidays/tests/AbstractTest.java
+++
@@ -1,8 +0,0 @@
-package com.mechadev.mechaholidays.tests;
-
-import java.text.SimpleDateFormat;
-
-public abstract class AbstractTest {
-	protected final int year = 1974;
-	protected SimpleDateFormat simpleDateFormat  = new SimpleDateFormat("MMMM dd, yyyy");
-}
